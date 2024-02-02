38308933a906b7c4fbe55233c53c8f81c74482b2
andre abreu <aanddre@gmail.com>
2024-02-01 22:32:22 -0300

    Spring Boot Rest API

---
+++ jdev.springboot/src/main/java/jdev/springboot/Application.java
@@ -0,0 +1,17 @@
+package jdev.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        
+    	SpringApplication.run(Application.class, args); //Linha principal que roda o projeto JAVA SpringBoot
+    	
+    }
+}
